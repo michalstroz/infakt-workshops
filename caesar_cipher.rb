2f60310504507f00005319571101db0697dce599
Michał <michalstroz@gmail.com>
2018-05-23 21:11:56 +0200

    finished workshops exercises

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,3 +1,18 @@
-class CeasarCipher
+class CaesarCipher
+  def initialize(str, n=13)
+    @str = str
+    @n = n
+    @miniscules = Array('a'..'z')
+    @majuscules = Array('A'..'Z')
+  end
 
+  def perform
+    @str.chars.map{ |v| maj_min_hash.fetch(v, v) }.join
+  end
+
+  private
+
+  def maj_min_hash
+    Hash[@miniscules.zip(@miniscules.rotate(@n))].merge(Hash[@majuscules.zip(@majuscules.rotate(@n))])
+  end
 end
--- passed_or_failed.rb
+++ passed_or_failed.rb
@@ -1,3 +1,21 @@
 class PassedOrFailed
+  def initialize(hash, threshold)
+    @hash = hash.inject({}){ |h, (k, v)| h[k] = v.to_i; h}
+    @threshold = threshold.to_i
+  end
 
+  def perform
+    create_hash
+  end
+
+  private
+
+  def group_studends
+    @hash.group_by{ |k, v| v >= @threshold ? :passed  : :failed }
+  end
+
+  def create_hash
+    group = group_studends
+    { passed: group[:passed].to_h, failed: group[:failed].to_h }
+  end
 end
